1031312f2cdf4e023235ff5647f5acc42e7d0470
Clinton Dreisbach <clinton@dreisbach.us>
2015-06-21 10:55:40 -0400

    Remove admin page
    Conflicts:
    	todomvc/todo/models.py

--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
