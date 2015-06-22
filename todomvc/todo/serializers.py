13ea21f9a2b49e4a44bc3d3ae8d24242c5cb1905
John Waldrep <johnwaldrep@gmail.com>
2015-06-22 13:34:44 -0400

    Add serializers.py

--- todomvc/todo/serializers.py
+++ todomvc/todo/serializers.py
@@ -1,1 +1,14 @@
-__author__ = 'jdw'
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('url', 'username', 'email', 'groups')
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'completed', 'order')
