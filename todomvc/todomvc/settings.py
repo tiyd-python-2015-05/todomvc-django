ba93a5927b3f497c02c332e013b42b8ea85d6259
John Waldrep <johnwaldrep@gmail.com>
2015-06-22 13:49:58 -0400

    Update settings

--- todomvc/todomvc/settings.py
+++ todomvc/todomvc/settings.py
@@ -105,7 +105,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    )
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.AllowAny',
+    # )
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+
 }
