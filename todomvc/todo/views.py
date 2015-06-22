f33ed2704a8b104ec89f80119e6f8234135abce9
Benjamin Phillips <benred42@gmail.com>
2015-06-22 13:41:04 -0400

    Made api for todo app

--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from todo.models import Todo
+from todo.serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
