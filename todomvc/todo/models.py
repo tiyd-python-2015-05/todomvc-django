5dea3ac7979cda6b4421116e447528ce638f75d4
Manish Patel <maddypatel@gmail.com>
2015-06-22 15:40:10 -0400

    Added models

--- todomvc/todo/models.py
+++ todomvc/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    completed = models.BooleanField(default=False)
+    order = models.IntegerField(null=True)
