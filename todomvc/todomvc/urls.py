dbf71ec0a5b93d20bbcf8ff76a5d2c52bce32893
John Waldrep <johnwaldrep@gmail.com>
2015-06-22 13:48:29 -0400

    Add routers and urls

--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from rest_framework import routers
+from todomvc.todo import views
 
+router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+router.register(r'todos', views.TodoViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
