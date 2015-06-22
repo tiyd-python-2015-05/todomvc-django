1699ca91ad6aba981a35772c7500c01cefc06929
Josh Higgins <jshhggns@gmail.com>
2015-06-22 15:00:02 -0400

    Fixed serializer to include ID

--- todomvc/todo/serializer.py
+++ todomvc/todo/serializer.py
@@ -5,5 +5,5 @@ from .models import Todo
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Todo
-        fields = ['title', 'completed', 'order']
+        fields = ['id', 'title', 'completed', 'order']
 
