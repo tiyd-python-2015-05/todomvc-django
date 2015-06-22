e9cf9b385271642f3d008091b1350d1b60a9d4de
Josh Higgins <jshhggns@gmail.com>
2015-06-22 14:35:11 -0400

    Normal Mode

---
+++ todomvc/todo/serializer.py
@@ -0,0 +1,9 @@
+__author__ = 'joshuahiggins'
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['title', 'completed', 'order']
+
--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -17,6 +17,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
+from todo import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'^api/todos', views.TodoViewSet)
+
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^', include(router.urls)),
 ]
+
