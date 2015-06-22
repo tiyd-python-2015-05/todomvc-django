2a31b1cac8425e865c48295bae5da60380d3c193
John Waldrep <johnwaldrep@gmail.com>
2015-06-22 13:45:45 -0400

    Add serializer

--- todomvc/todo/serializers.py
+++ todomvc/todo/serializers.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from todo.models import Todo
 
 # class UserSerializer(serializers.HyperlinkedModelSerializer):
 #     class Meta:
 #         model = User
 #         fields = ('url', 'username', 'email', 'groups')
+
 
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets
+# from django.contrib.auth.models import User, Group
+# from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from todomvc.todo.serializers import TodoSerializer
 
-# Create your views here.
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited.
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
